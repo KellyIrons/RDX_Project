db2b0a81e2cf9349634833e2caa2ef3ba14ecb04
KellyIrons <79431051+KellyIrons@users.noreply.github.com>
2022-01-17 13:16:13 -0500

    Add files via upload
    Fixed indexing errors

--- plot_one_species.py
+++ plot_one_species.py
@@ -5,6 +5,7 @@ Plot One Species
 def plot_one_species(species, Slist, TIME_o, solutionM_o, TIME_f, solutionM_f, mechobj):
     import matplotlib.pylab as plt
         
+    ''' 
     species_names = mechobj['species_names']
     for new_ind in species:
         old_ind = species_names.index(Slist[new_ind]['name'])
@@ -16,3 +17,23 @@ def plot_one_species(species, Slist, TIME_o, solutionM_o, TIME_f, solutionM_f, m
         plt.grid(True)
         plt.legend()
         plt.show()
+    '''
+    
+    from RDXskeletalparser import RDXskeletalparser
+    [old_Rlist, old_Slist, old_lg] = RDXskeletalparser()
+    
+    all_species_names = [x['name'] for x in old_Slist]
+    
+    
+
+    for new_ind in species:
+        old_ind = all_species_names.index(Slist[new_ind]['name'])
+        plt.plot(TIME_o, solutionM_o[:,old_ind], 'b-', label = 'Original Mechanism')  
+        plt.plot(TIME_f, solutionM_f[:,new_ind], 'r:', label = 'Reduced Mechanism')
+        plt.xlabel('Time (s)')    
+        plt.ylabel('Mass Fraction')
+        plt.title('%s Results Comparison' % Slist[new_ind]['name'])
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+    
